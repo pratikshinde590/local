55e8868fcbfefca63b3d1e9ebe186e7e9d282f1a
Vikas Sharma <13715661+vikas3045@users.noreply.github.com>
2023-11-23 18:44:49 +0530

    Added login screen

--- src/cryptobot_frontend/src/components/menu/NavBar.jsx
+++ src/cryptobot_frontend/src/components/menu/NavBar.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useNavigate, Link } from "react-router-dom";
 import { Button, Dropdown } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import NewInvestment from "../../modals/Investment-modal/NewInvestment.modal";
+import { AuthClient } from '@dfinity/auth-client';
 
 // import profile from "../../../assets/Ellipse 64.svg";
 
 
 const NavBar = () => {
+    const navigate = useNavigate();
+    const [authClient, setAuthClient] = useState();
+    const [userIdentity, setUserIdentity] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const authClient = await AuthClient.create();
+            setAuthClient(authClient);
+
+            if (await authClient.isAuthenticated()) {
+                handleAuthenticated(authClient);
+            }
+        })();
+    }, []);
+
+    const handleAuthenticated = async (authClient) => {
+        const identity = authClient.getIdentity();
+        setUserIdentity(identity);
+    };
+
+    const handleLogout = async () => {
+        if (!authClient) return;
+
+        // Sign out of Internet Identity
+        await authClient.logout();
+        setUserIdentity(null);
+        navigate("/");
+    };
+
     return (
         <>
             {/* A "layout route" is a good place to put markup you want to
@@ -16,22 +46,16 @@ const NavBar = () => {
             <div className="d-flex flex-row justify-content-between px-5 py-2 bg-white">
                 <Nav variant="underline" defaultActiveKey="/">
                     <Nav.Item>
-                        <Link className="nav-link" to="/">Home</Link>
-                    </Nav.Item>
-                    <Nav.Item>
                         <Link className="nav-link" to="/dashboard">Dashboard</Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Link className="nav-link" to="/investments">Investments</Link>
                     </Nav.Item>
-                    <Nav.Item>
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </Nav.Item>
                 </Nav>
                 <div className=" d-flex flex-row gap-2">
-                    <Button className="rounded-5 px-4 " variant="primary" size="sm">
+                    {/* <Button className="rounded-5 px-4 " variant="primary" size="sm">
                         Connect Wallet
-                    </Button>
+                    </Button> */}
                     
                     <NewInvestment />
 
@@ -41,13 +65,13 @@ const NavBar = () => {
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1" active>
+                            {/* <Dropdown.Item href="#/action-1" active>
                                 Action
                             </Dropdown.Item>
                             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                            <Dropdown.Divider /> */}
+                            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </div>
