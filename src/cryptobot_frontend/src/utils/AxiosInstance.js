183419c782ea6a73ef1c97ab97770472a426110f
Vikas Sharma <13715661+vikas3045@users.noreply.github.com>
2023-11-29 14:13:51 +0530

    Update AxiosInstance.js

--- src/cryptobot_frontend/src/utils/AxiosInstance.js
+++ src/cryptobot_frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseUrl = process.env.NODE_ENV === 'development'? 'http://127.0.0.1:5000/':'https://cryptoflask.azurewebsites.net/';
+console.log("BaseURL being used:", baseUrl );
+
 const instance = axios.create({
-    baseURL : "http://127.0.0.1:5000",
-})
+    baseURL: process.env.NODE_ENV === 'development'? 'http://127.0.0.1:5000/':'https://cryptoflask.azurewebsites.net/',
+});
 
 export default instance
